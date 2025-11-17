31610eb734058b8bd60d3fce3f3d40643f9d7073
rvmiranda <rafmicr884@gmail.com>
2025-11-16 18:15:28 -0600

    cambio para actions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -86,7 +86,7 @@ export default function App(){
 				{activeTab === "calculator" && <Calculator />}
 				{activeTab === "history" && <History />}
 			</div>
-			{/* comentario para hacer build */}
+			{/* comentario para levnatar imagen desde actions y hacer build */}
 		</div>
 	);
 }
